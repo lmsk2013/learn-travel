c9ec0aa5644d947d5f6b6e1078935626a183923b
袁振 <lmsk2013@outlook.com>
2020-02-21 13:53:40 +0800

    城市页布局与better-scroll使用

---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,104 @@
+<template>
+  <!-- better-scroll要求必须有两层包裹滚动项
+       ref用来获取dom元素以绑定滚动区域 -->
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">济南</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">济南</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">济南</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">济南</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+          <div class="item border-bottom">阿拉尔</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// 引入better-scroll
+import Bsrcoll from 'better-scroll'
+export default {
+  name: 'CityList',
+  mounted () {
+    // 在页面渲染时，初始化滚动对象
+    this.scroll = new Bsrcoll(this.$refs.wrapper)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl';
+  .border-topbottom
+    &:before
+      border-color #ccc
+    &:after
+      border-color #ccc
+  .border-bottom
+    &:before
+      border-color #ccc
+  .list
+    // 使得整个list区域位置固定，不能滚动，交由插件实现滚动效果
+    overflow hidden
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+    .title
+      line-height .54rem
+      background #eee
+      padding-left .2rem
+      color #666
+      font-size .26rem
+    .button-list
+      overflow hidden
+      padding .1rem .6rem .1rem .1rem
+      .button-wrapper
+        float left
+        width 33.33%
+        .button
+          margin .1rem
+          padding .1rem
+          text-align center
+          border .02rem solid #ccc
+          border-radius .06rem
+    .item-list
+      .item
+        line-height .76rem
+        padding-left .2rem
+</style>
